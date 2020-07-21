66f7833019a07158d71080641d2563614fd2b727
Xinyi Samore <44687589+claireche89@users.noreply.github.com>
2020-07-21 14:16:28 -0500

    Initial commit

---
+++ codetorunshinyapps.R
@@ -0,0 +1,5 @@
+library(shiny)
+setwd("D:/level/capstone/Kungfu Data")
+# This is the directory where you have downloaded and unzipped your app.
+runApp("salesvolume")
+runApp("topproductscatterplot")
